65636118fd457091180e0ddfa84937a0b9966030
harko83 <arthurdiet@hotmail.fr>
2019-11-08 10:40:44 +0100

    some change

--- SudokuChallenge/src/sudoku/view/Flybox.java
+++ SudokuChallenge/src/sudoku/view/Flybox.java
@@ -8,20 +8,22 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSeparator;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 public class Flybox extends JFrame {
 	
 	private static final long serialVersionUID = 1L;
-
+	//variables
 	JButton btnOk, btnCancelButton;
-	GridBagConstraints gbc = new GridBagConstraints();
-	
 	Image img;
-	
 	JLabel lbJeu, lbNb, lbInstr, imgNumber;
-	
 	JTextField jTextField;
+	JSeparator jSep;
+	
+	GridBagConstraints gbc = new GridBagConstraints();
 	
 	public Flybox() {
 		this.setSize(500, 250);
@@ -30,58 +32,85 @@ public class Flybox extends JFrame {
 		this.setResizable(true);
 		this.setLayout(new GridBagLayout());
 		
-		gbc.anchor = GridBagConstraints.FIRST_LINE_START;
-		//gbc.fill = GridBagConstraints.HORIZONTAL;
+		//gbc.anchor = GridBagConstraints.FIRST_LINE_START;
+		
+		gbc.fill = GridBagConstraints.BOTH;
 		
 		////////////image ////////////////
 		imgNumber = new JLabel();
 		imgNumber.setIcon(new ImageIcon("src\\sudoku\\view\\1.png"));
-		gbc.gridwidth = 2;
 		gbc.gridx = 0;
 		gbc.gridy = 0;
-
-	
+		gbc.gridwidth = 1;
+		gbc.gridheight = 2;
+		gbc.weightx = 1.0;
+		gbc.weighty = 1.0;
 		this.add(imgNumber, gbc);
 		
 		/////////////Label//////////////
 		lbJeu = new JLabel("SUDOKU");
-		gbc.gridwidth = 0;
 		gbc.gridx = 2;
 		gbc.gridy = 0;
+		gbc.gridwidth = 2;
+		gbc.gridheight = 1;
+		gbc.weightx = 1.0;
+		gbc.weighty = 1.0;
 		this.add(lbJeu, gbc);
 		
 		lbNb = new JLabel("Version Oiseaux");
-		gbc.gridwidth = 0;
 		gbc.gridx = 2;
 		gbc.gridy = 1;
+		gbc.gridwidth = 2;
+		gbc.gridheight = 1;
+		gbc.weightx = 1.0;
+		gbc.weighty = 1.0;
 		this.add(lbNb, gbc);
 		
-		//this.add(new JSeparator());
+		jSep = new JSeparator(SwingConstants.HORIZONTAL);
+		gbc.gridx = 2;
+		gbc.gridy = 2;
+		gbc.gridwidth = 3;
+		gbc.gridheight = 1;
+		gbc.weightx = 1.0;
+		gbc.weighty = 1.0;
+		this.add(jSep, gbc);
 		
 		lbInstr = new JLabel("Veuillez entrer un chiffre : ");
-		gbc.gridwidth = 0;
 		gbc.gridx = 2;
 		gbc.gridy = 3;
+		gbc.gridwidth = 1;
+		gbc.gridheight = 1;
+		gbc.weightx = 1.0;
+		gbc.weighty = 1.0;
 		this.add(lbInstr, gbc);
 		
 		//////////JTextfield/////////
 		jTextField = new JTextField(" ");
-		gbc.gridwidth = 0;
 		gbc.gridx = 3;
 		gbc.gridy = 3;
+		gbc.gridwidth = 1;
+		gbc.gridheight = 1;
+		gbc.weightx = 1.0;
+		gbc.weighty = 1.0;
 		this.add(jTextField, gbc);
 		
 		//////////// boutons /////////////
 		btnOk = new JButton("OK");
-		gbc.gridwidth = 0;
 		gbc.gridx = 2;
 		gbc.gridy = 4;
+		gbc.gridwidth = 1;
+		gbc.gridheight = 1;
+		gbc.weightx = 1.0;
+		gbc.weighty = 1.0;
 		this.add(btnOk, gbc);
 		
 		btnCancelButton = new JButton("Cancel");
-		gbc.gridwidth = 0;
 		gbc.gridx = 3;
 		gbc.gridy = 4;
+		gbc.gridwidth = 1;
+		gbc.gridheight = 1;
+		gbc.weightx = 1.0;
+		gbc.weighty = 1.0;
 		this.add(btnCancelButton, gbc);
 		
 		this.setUndecorated(false);
